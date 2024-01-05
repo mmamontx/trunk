756627787f983beb1b0a5821b2abd9a1b62e0be7
Mikhail Mamontov <mmamontx@gmail.com>
2024-01-05 11:57:38 +0300

    Cleanup.

--- rage-nov-21-final/rage.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-LD_LIBRARY_PATH=/home/rage/source /home/rage/rage &
-/root/MimicKShM_25_aug/build/MimicKShM >/dev/null 2>&1
--- rage-nov-21-final/rc.h
+++
@@ -1,8 +0,0 @@
-#ifndef _RC_H
-#define _RC_H 1
-
-extern int rc_connect();
-extern int rc_offitem(const unsigned char, const unsigned char);
-extern void *rc(void *arg);
-
-#endif /* !_RC_H */
